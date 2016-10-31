9f5580d96f6372433902c838d15f14b868d3a23d
Vanessa Esther Pacheco De La Rosa <t00036318@utbvirtual.edu.co>
2016-10-30 21:00:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/windows7/lasesquinasdemiciudad/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.windows7.lasesquinasdemiciudad;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    int aleatorio;
+    String foto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        aleatorio = new Double(Math.random() * 100).intValue();
+        foto = Environment.getExternalStorageDirectory() + "/imagen"+ aleatorio +".jpg";
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action1) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Uri output = Uri.fromFile(new File(foto));
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, output);
+            startActivityForResult(intent, 1);
+        }
+
+        if (id == R.id.action2) {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, 1);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
